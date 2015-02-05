2eefd312d301c194ccddd019619fd522f16c13c0
Gamer1120 <michaelkoopmantest@gmail.com>
2015-02-05 09:30:17 +0100

    MyProxy User-Agent

--- src/week1/proxy/MyProxy.java
+++ src/week1/proxy/MyProxy.java
@@ -6,55 +6,63 @@ import java.util.*;
 
 public class MyProxy extends PrivacyProxy {
 
-    //////////////////////////////////////////////////////////////////////////
-    //
-    // Enhance your proxy by implementing the following three methods:
-    //   - manipulateRequestHeaders
-    //   - onRequest
-    //   - onResponse
-    //
-    //////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	//
+	// Enhance your proxy by implementing the following three methods:
+	//   - manipulateRequestHeaders
+	//   - onRequest
+	//   - onResponse
+	//
+	//////////////////////////////////////////////////////////////////////////
 
-    protected HashMap<String, String> onRequest(HashMap<String, String> requestHeaders){
+	public static final String USERAGENT = "User-Agent";
 
-        // print all the request headers 
-        for (String header : requestHeaders.keySet()) {
-            log("  REQ: " + header + ": " + requestHeaders.get(header));
-        }
+	protected HashMap<String, String> onRequest(
+			HashMap<String, String> requestHeaders) {
 
-        return requestHeaders;
+		// print all the request headers 
+		for (String header : requestHeaders.keySet()) {
+			switch (header) {
+			case USERAGENT:
+				requestHeaders.put("User-Agent", "");
+				break;
+			}
+			log("  REQ: " + header + ": " + requestHeaders.get(header));
+		}
 
-        // return the (manipulated) headers, or
-        // alternatively, drop this request by returning null
-        // return null;
-    }
+		return requestHeaders;
 
+		// return the (manipulated) headers, or
+		// alternatively, drop this request by returning null
+		// return null;
+	}
 
-    // The number of valid bytes in the buffer is expressed by the inOctets instance variable
-    // e.g. log("I received " + this.inOctets + " bytes");
-    protected byte[] onResponse(byte[] originalBytes){
-        byte[] alteredBytes = originalBytes;
-        log("I received " + this.inOctets + " bytes");
+	// The number of valid bytes in the buffer is expressed by the inOctets instance variable
+	// e.g. log("I received " + this.inOctets + " bytes");
+	protected byte[] onResponse(byte[] originalBytes) {
+		byte[] alteredBytes = originalBytes;
+		log("I received " + this.inOctets + " bytes");
 
-        for (String header : responseHeaders.keySet()) {
-            log("  RSP: " + header + ": " + responseHeaders.get(header));
+		for (String header : responseHeaders.keySet()) {
+			log("  RSP: " + header + ": " + responseHeaders.get(header));
 
-            if (header.equals("Content-Type") && responseHeaders.get("Content-Type").startsWith("text/html")) {
-               //String s = new String(originalBytes);
-               //String s2 = s.replaceAll("Nieuws", "Nieuws!");
-               //alteredBytes = s2.getBytes();
-            }
+			if (header.equals("Content-Type")
+					&& responseHeaders.get("Content-Type").startsWith(
+							"text/html")) {
+				//String s = new String(originalBytes);
+				//String s2 = s.replaceAll("Nieuws", "Nieuws!");
+				//alteredBytes = s2.getBytes();
+			}
 
-        }
+		}
 
-        // alter the original response and return it
-        
-        return alteredBytes;
-    }
+		// alter the original response and return it
 
-    
-    // Constructor, no need to touch this
-    public MyProxy(Socket socket, Boolean autoFlush) {
-        super(socket, autoFlush);
-    }
+		return alteredBytes;
+	}
+
+	// Constructor, no need to touch this
+	public MyProxy(Socket socket, Boolean autoFlush) {
+		super(socket, autoFlush);
+	}
 }
